f109a5a08d4a6d0b10a9622bf1e430783b785a75
Marjuk Ahmed Siddiki <36816925+Marjuk-Ahmed-Siddiki@users.noreply.github.com>
2019-11-23 18:06:50 +0600

    Add files via upload

---
+++ section_database.c
@@ -0,0 +1,365 @@
+#include<stdio.h>
+
+
+void Marjuk()
+{
+    printf("\n:::Department of CSE:::\nName: Marjuk Ahmed Siddiki\nRoll: 171-15-8959\nCGPA: 4.00\n");
+}
+void Niloy()
+{
+    printf("\n:::Department of CSE:::\nName: Niloy Chandra Shil\nRoll: 171-15-9343\nCGPA: 4.00\n");
+}
+void Monjur()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Monjur Hasan Milton\nRoll: 171-15-9220\nCGPA: 4.00\n");
+}
+void Omar()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Omar Faruk\nRoll: 171-15-9114\nCGPA: 4.00\n");
+}
+void Ishtiak()
+{
+    printf("\n:::Department of CSE:::\nName: Ishtiak Ahmed\nRoll: 171-15-9062\nCGPA: 4.00\n");
+}
+void Foysal()
+{
+    printf("\n:::Department of CSE:::\nName: Foysal Ahmed\nRoll: 171-15-9041\nCGPA: 4.00\n");
+}
+void Ariful()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Ariful Haque\nRoll: 171-15-9037\nCGPA: 4.00\n");
+}
+void Mehedi()
+{
+    printf("\n:::Department of CSE:::\nName: Mehedi Hassan\nRoll: 171-15-8983\nCGPA: 4.00\n");
+}
+void Sumayia()
+{
+    printf("\n:::Department of CSE:::\nName: Sumayia Islam\nRoll: 171-15-8976\nCGPA: 4.00\n");
+}
+void Jahedul()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Jahedul Islam Joy\nRoll: 171-15-8944\nCGPA: 4.00\n");
+}
+void Shahadat()
+{
+    printf("\n:::Department of CSE:::\nName: Shahadat Hossain\nRoll: 171-15-8938\nCGPA: 4.00\n");
+}
+void Jesmin()
+{
+    printf("\n:::Department of CSE:::\nName: Jesmin Akter\nRoll: 171-15-8918\nCGPA: 4.00\n");
+}
+void Jamsedul()
+{
+    printf("\n:::Department of CSE:::\nName: Jamsedul Islam\nRoll: 171-15-8889\nCGPA: 4.00\n");
+}
+void Farhad()
+{
+    printf("\n:::Department of CSE:::\nName: Farhad Hosan Santo\nRoll: 171-15-8879\nCGPA: 4.00\n");
+}
+void Abu()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Abu Hasan Sayem\nRoll: 171-15-8868\nCGPA: 4.00\n");
+}
+void Marajul()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Marajul Islam\nRoll: 171-15-8806\nCGPA: 4.00\n");
+}
+void Naim()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Naim Hossain\nRoll: 171-15-8796\nCGPA: 4.00\n");
+}
+void Mahedi()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Mehedi Hassan\nRoll: 171-15-8793\nCGPA: 4.00\n");
+}
+void Saikat()
+{
+    printf("\n:::Department of CSE:::\nName: Saikat Mazumder\nRoll: 171-15-8791\nCGPA: 4.00\n");
+}
+void Jahidul()
+{
+    printf("\n:::Department of CSE:::\nName: Jahidul Islam Akash\nRoll: 171-15-8786\nCGPA: 4.00\n");
+}
+void Adnan()
+{
+    printf("\n:::Department of CSE:::\nName: Adnan Shahriar\nRoll: 171-15-8750\nCGPA: 4.00\n");
+}
+void Mostafijur()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Mostafijur Rahman\nRoll: 171-15-8741\nCGPA: 4.00\n");
+}
+void Aminul()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Aminul Islam Rabby\nRoll: 171-15-8729\nCGPA: 4.00\n");
+}
+void Oamar()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Omar Faruk\nRoll: 171-15-8709\nCGPA: 4.00\n");
+}
+void Zahid()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Zahid Hasan Sohan\nRoll: 171-15-8697\nCGPA: 4.00\n");
+}
+void Tithi()
+{
+    printf("\n:::Department of CSE:::\nName: Tithi Sutradhar\nRoll: 171-15-8695\nCGPA: 4.00\n");
+}
+void Kuheli()
+{
+    printf("\n:::Department of CSE:::\nName: Kuheli Ahsan\nRoll: 171-15-8680\nCGPA: 4.00\n");
+}
+void Fuad()
+{
+    printf("\n:::Department of CSE:::\nName: Fuad Hossain\nRoll: 171-15-8674\nCGPA: 4.00\n");
+}
+void Shohanur()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Shohanur Rahman Shohan\nRoll: 171-15-8672\nCGPA: 4.00\n");
+}
+void Israt()
+{
+    printf("\n:::Department of CSE:::\nName: Israt Jahan Jemi\nRoll: 171-15-8655\nCGPA: 4.00\n");
+}
+void Kadery()
+{
+    printf("\n:::Department of CSE:::\nName: Kadery Janna Chad\nRoll: 171-15-8652\nCGPA: 4.00\n");
+}
+void Monjurur()
+{
+    printf("\n:::Department of CSE:::\nName: Monjurur Kader Shipu\nRoll: 171-15-8644\nCGPA: 4.00\n");
+}
+void Faisal()
+{
+    printf("\n:::Department of CSE:::\nName: Faisal Al Mamun\nRoll: 171-15-8628\nCGPA: 4.00\n");
+}
+void Shoeb()
+{
+    printf("\n:::Department of CSE:::\nName: Shoeb Islam Shaon\nRoll: 171-15-8614\nCGPA: 4.00\n");
+}
+void Yasin()
+{
+    printf("\n:::Department of CSE:::\nName: Yasin Anis\nRoll: 171-15-8588\nCGPA: 4.00\n");
+}
+void Khadija()
+{
+    printf("\n:::Department of CSE:::\nName: Khadija Akther\nRoll: 171-15-8587\nCGPA: 4.00\n");
+}
+void Umme()
+{
+    printf("\n:::Department of CSE:::\nName: Umme Salma\nRoll: 171-15-8578\nCGPA: 4.00\n");
+}
+void Sakib()
+{
+    printf("\n:::Department of CSE:::\nName: Sakib Ibn Abdullah\nRoll: 171-15-8575\nCGPA: 4.00\n");
+}
+void Soliman()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Soliman Ali\nRoll: 171-15-8572\nCGPA: 4.00\n");
+}
+void Khairul()
+{
+    printf("\n:::Department of CSE:::\nName: Md. Khairul Bashar\nRoll: 171-15-8569\nCGPA: 4.00\n");
+}
+void Sanjida()
+{
+    printf("\n:::Department of CSE:::\nName: Sanjida Khanam\nRoll: 171-15-8568\nCGPA: 4.00\n");
+}
+void Rony()
+{
+    printf("\n:::Department of CSE:::\nName: Minhajul Islam Rony\nRoll: 171-15-8519\nCGPA: 4.00\n");
+}
+void Sagar()
+{
+    printf("\n:::Department of CSE:::\nName: Sagar Gosh\nRoll: 171-15-8518\nCGPA: 4.00\n");
+}
+
+
+
+void main()
+{
+    int a;
+    printf("    Daffodil International University\n");
+    printf("::::::::::Welcome to our Section (J)::::::::::\n");
+    printf("Please enter your roll no:......(Last 4 digit)\n");
+    scanf("%d",&a);
+
+
+    if(a==8959)
+    {
+        Marjuk();
+    }
+    else if(a==8518)
+    {
+        Sagar();
+    }
+    else if(a==8519)
+    {
+        Rony();
+    }
+    else if(a==8568)
+    {
+        Sanjida();
+    }
+    else if(a==8569)
+    {
+        Khairul();
+    }
+    else if(a==8572)
+    {
+        Soliman();
+    }
+    else if(a==8575)
+    {
+        Sakib();
+    }
+    else if(a==8578)
+    {
+        Umme();
+    }
+    else if(a==8587)
+    {
+        Khadija();
+    }
+    else if(a==8588)
+    {
+        Yasin();
+    }
+    else if(a==8614)
+    {
+        Shoeb();
+    }
+    else if(a==8628)
+    {
+        Faisal();
+    }
+    else if(a==8644)
+    {
+        Monjurur();
+    }
+    else if(a==8652)
+    {
+        Kadery();
+    }
+    else if(a==8655)
+    {
+        Israt();
+    }
+    else if(a==8672)
+    {
+        Shohanur();
+    }
+    else if(a==8674)
+    {
+        Fuad();
+    }
+    else if(a==8680)
+    {
+        Kuheli();
+    }
+    else if(a==8695)
+    {
+        Tithi();
+    }
+    else if(a==8697)
+    {
+        Zahid();
+    }
+    else if(a==8709)
+    {
+        Oamar();
+    }
+    else if(a==8729)
+    {
+        Aminul();
+    }
+    else if(a==8741)
+    {
+        Mostafijur();
+    }
+    else if(a==8750)
+    {
+        Adnan();
+    }
+    else if(a==8786)
+    {
+        Jahidul();
+    }
+    else if(a==8791)
+    {
+        Saikat();
+    }
+    else if(a==8793)
+    {
+        Mahedi();
+    }
+    else if(a==8796)
+    {
+        Naim();
+    }
+    else if(a==8806)
+    {
+        Marajul();
+    }
+    else if(a==8868)
+    {
+        Abu();
+    }
+    else if(a==8879)
+    {
+        Farhad();
+    }
+    else if(a==8889)
+    {
+        Jamsedul();
+    }
+    else if(a==8918)
+    {
+        Jesmin();
+    }
+    else if(a==8938)
+    {
+        Shahadat();
+    }
+    else if(a==8944)
+    {
+        Jahedul();
+    }
+    else if(a==8976)
+    {
+        Sumayia();
+    }
+    else if(a==8983)
+    {
+        Mehedi();
+    }
+    else if(a==9037)
+    {
+        Ariful();
+    }
+    else if(a==9041)
+    {
+        Foysal();
+    }
+    else if(a==9062)
+    {
+        Ishtiak();
+    }
+    else if(a==9114)
+    {
+        Omar();
+    }
+    else if(a==9220)
+    {
+        Monjur();
+    }
+    else if(a==9343)
+    {
+        Niloy();
+    }
+    else
+    {
+        printf("Wrong roll number....Please try again\n");
+    }
+
+}
